2b4b806507a27ea95a66cb5f51c9afd61b1c8a83
Timo Sand <timo.j.sand@gmail.com>
2013-11-02 11:54:50 +0200

    Added event creation

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,6 +7,16 @@ class EventsController < ApplicationController
   end
 
   def create
+    @event = Event.new(event_params)
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to events_url, notice: 'Thank you for your order.' }
+        format.json { render json: @event, status: :created, location: @event }
+      else
+        format.html { render :new }
+        format.json { render json: @event.errors, status: 'Failure' }
+      end
+    end
   end
 
   def update
@@ -15,4 +25,13 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
   end
+
+  def new
+    @event = Event.new
+  end
+
+  private
+    def event_params
+      params.require(:event).permit(:name, :created_by, :location, :deadline)
+    end
 end
