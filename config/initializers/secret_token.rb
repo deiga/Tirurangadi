0e142ea4571310f11f538e95ea0795d4a8eee357
Timo Sand <timo.j.sand@gmail.com>
2013-11-02 10:18:09 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tirurangadi::Application.config.secret_key_base = 'dea2f68ea0c7cb65f85ce43f22d4dac04689c52e24821921dc00656fa0224abb343ac8493b4f58a729e9bdd0d69bb5c6e443b887c10d9e6768ceb86e878d66db'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tirurangadi::Application.config.session_store :cookie_store, key: '_Tirurangadi_session'
