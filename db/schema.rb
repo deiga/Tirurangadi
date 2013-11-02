b1a81729242055ea543395b09f6e9a70e9d23e67
Timo Sand <timo.j.sand@gmail.com>
2013-11-02 14:35:41 +0200

    Updated db

--- db/schema.rb
+++ db/schema.rb
@@ -15,14 +15,20 @@ ActiveRecord::Schema.define(version: 20131102113652) do
 
   create_table "attendances", force: true do |t|
     t.integer  "candidate_id"
+    t.integer  "attendee_id"
     t.boolean  "data"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "attendee"
   end
 
-  add_index "attendances", ["attendee"], name: "index_attendances_on_attendee"
+  add_index "attendances", ["attendee_id"], name: "index_attendances_on_attendee_id"
   add_index "attendances", ["candidate_id"], name: "index_attendances_on_candidate_id"
+
+  create_table "attendees", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "candidates", force: true do |t|
     t.string   "title"
